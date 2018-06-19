43422d194912d2c2c8834aa885776b976da3c241
brandonchaffee <brandon@patterncapture.com>
2018-06-19 16:45:37 -0700

    fixed for solium

--- contracts/Demopera.sol
+++ contracts/Demopera.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.23;
 
-import './sections/Organization.sol';
-import './sections/Project.sol';
-import './sections/Submission.sol';
-import './sections/Contribution.sol';
+import "./sections/Organization.sol";
+import "./sections/Project.sol";
+import "./sections/Submission.sol";
+import "./sections/Contribution.sol";
 
 contract Demopera is Organization, Project, Submission, Contribution  {
     constructor(uint256 supply) public {
